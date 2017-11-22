9b80794c448c764585231cb79c79397198bf5f44
JumpLight <boomwyh@sina.com>
2017-11-22 11:51:56 +0800

    Update KDTree.cpp

--- KDTree.cpp
+++ KDTree.cpp
@@ -1,19 +1,24 @@
 #include "KDTree.h"
 #include "Utility.h"
-#include <iostream>
-#include <vector>
-using namespace std;
-
 /*在这里实现KD树的各个函数*/
 
 KDTree::KDTree(){
 	vector<float> v;
 	root = NULL;
 	count = 0;
-	dist = 0;
+	dist = INT_MAX;
 }
 
 KDTree::~KDTree(){}
+
+int KDTree::countNode(Node *root){
+	int nodes = 0;
+        if(root == NULL)
+            return 0;
+        else
+            nodes = 1 + countNode(root->left) + countNode(root->right);
+    return nodes;
+}
 void KDTree::_storeTree(const Node *ptr, int pos){
 	if(!ptr)
 		return;
@@ -27,7 +32,9 @@ void KDTree::_storeTree(const Node *ptr, int pos){
 }
 
 bool KDTree::storeTree(const char* index_path){
-	filePtr = fopen(index_path, "wb");
+	char storeTree[L];
+	sprintf(storeTree, "%s/storeTree.txt", index_path);
+	filePtr = fopen(storeTree, "wb");
 	_storeTree(root, 1);
 	fclose(filePtr);
 	return true;
@@ -37,8 +44,9 @@ bool KDTree::restoreTree(const char* index_path){
 	map<int, Node*> nodeMap;
 	Node *ptr;
 	fileNode fNode;
-	filePtr = fopen(index_path, "rb");
-	
+	char storeTree[L];
+	sprintf(storeTree, "%s/storeTree.txt", index_path);
+	filePtr = fopen(storeTree, "rb");
 	while(fread(&fNode, sizeof(fNode), (size_t)1, filePtr)>0){
 		ptr = new Node(fNode.mid, fNode.dimen, fNode.value);
 		nodeMap.insert(map<int, Node*>::value_type(fNode.pos, ptr));
@@ -76,8 +84,13 @@ bool KDTree::buildTree(int n, int d, float** data){
 			matrix[i][j] = data[j][i];
 		}
 	}
+	cout << "Building k-d tree..." << endl;
+	clock_t startTime, endTime;
+	startTime = clock();
 	_buildTree(d, root, matrix);
+	endTime = clock();
 	cout << "Build Complete" << endl;
+	cout<<"Build time: "<<(double)(endTime-startTime)/CLOCKS_PER_SEC<<"s"<<endl;
 	return true;
 }
 
@@ -98,13 +111,8 @@ void KDTree::_buildTree(int d, Node* &nodes, vector<vector<float> > list){
 
 	vector<vector<float> > leftlist(d+1, vector<float>());
 	vector<vector<float> > rightlist(d+1, vector<float>());
-	float mid = getMid(nodes, dimen, list, leftlist, rightlist, d);
-	/*for (int i = 0; i < d; i++){
-		cout << nodes->value[i] << " ";
-	}
-	cout << endl;*/
-	//cout << nodes->value[50] << endl;
-	//cout << leftlist[0].size() << " " << rightlist[0].size() << endl;
+	
+	split(nodes, dimen, list, leftlist, rightlist, d);
 	
 	_buildTree(d, nodes->left, leftlist);
 	_buildTree(d, nodes->right, rightlist);
@@ -113,44 +121,50 @@ void KDTree::_buildTree(int d, Node* &nodes, vector<vector<float> > list){
 int KDTree::_search(vector<float> query, Node* tree, int d){
 	if (tree == NULL){
 		dist = INT_MAX;
-		return NULL;
+		return 0;
 	}
-	vector<float> nearest(d+1);
+	dist = INT_MAX;
 	Node* point = tree;
-	nearest = point->value;
+	Node* nearest;
+	//first traverse
 	while (point){
 		searchPath.push(point);
-		if (getDistance(query, point->value) < getDistance(query, nearest)){
-			nearest = point->value;
-			dist = getDistance(query, point->value);
-		}
 		int dimen = point->dimen;
 		if (query[dimen] <= point->mid)
 			point = point->left;
 		else
 			point = point->right;
 	}
-	
+	//backtrack steps
 	Node* back;
 	while (!searchPath.empty()){
 		back = searchPath.top();
 		searchPath.pop();
+		float tempD = getDistance(query, back->value);
+		if (tempD < dist){
+			nearest = back;
+			dist = tempD;
+		}
 		int dimen = back->dimen;
-		if (fabs(back->mid - query[dimen]) < dist){
+		//if the circle and hyper-space intersect, enter the other child
+		//if(fabs(back->mid - query[dimen]) < sqrt(dist)){
 			if (query[dimen] <= back->mid)
 				point = back->right;
 			else
 				point = back->left;
-			if (point != NULL)
-				searchPath.push(point);
-		}
-		if (point != NULL)
-			if (getDistance(query, point->value) < getDistance(query, nearest)){
-				nearest = point->value;
-				dist = getDistance(query, point->value);
+			if(point){
+				while (point){
+					searchPath.push(point);
+					int dimen = point->dimen;
+					if (query[dimen] <= point->mid)
+						point = point->left;
+					else
+						point = point->right;
+				}
 			}
+		//}
 	}
-	return nearest[d];
+	return nearest->value[d];
 }
 
 int KDTree::getDimen(vector<vector<float> > v, int n){
@@ -174,21 +188,24 @@ int KDTree::getDimen(vector<vector<float> > v, int n){
 	return dimen;
 }
 
-float KDTree::getMid(Node* &nodes, int dimen, vector<vector<float> > list, vector<vector<float> > &leftlist, vector<vector<float> > &rightlist, int d){
+void KDTree::split(Node* &nodes, int dimen, vector<vector<float> > list, vector<vector<float> > &leftlist, vector<vector<float> > &rightlist, int d){
 	int pos = list[dimen].size() / 2;
 	//combine values in the dimension and corresponding index
 	vector<pair<float, float> > p;
 	for (int i = 0; i < list[dimen].size(); i++){
 		p.push_back(make_pair(list[dimen][i], list[d][i]));
 	}
-	sort(p.begin(), p.end());
+	nth_element(p.begin(),p.begin()+pos,p.end());
 	float mid = p[pos].first;
 	float pos1 = p[pos].second;
+	p.clear();
 	//get median position in list
 	int pos2 = 0;
 	for (int i = 0; i < list[dimen].size(); i++){
-		if (pos1 == list[d][i])
+		if (pos1 == list[d][i]){
 			pos2 = i;
+			break;
+		}
 	}
 	vector<float> value(d+1, 0);
 	for (int i = 0; i < d+1; i++){
@@ -196,16 +213,14 @@ float KDTree::getMid(Node* &nodes, int dimen, vector<vector<float> > list, vecto
 	}
 	//set node value
 	nodes = new Node(mid, dimen, value);
-
 	for (int i = 0; i < list[dimen].size(); i++){
 		if (list[d][i] != list[d][pos2]){
 			if (list[dimen][i] <= mid)
 				for (int j = 0; j < d+1; j++)
 					leftlist[j].push_back(list[j][i]);
-			else if (list[dimen][i] > mid)
+			else
 				for (int j = 0; j < d+1; j++)
 					rightlist[j].push_back(list[j][i]);
 		}
 	}
-	return mid;
 }
